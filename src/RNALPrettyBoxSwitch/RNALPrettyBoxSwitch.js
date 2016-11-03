504637ae77a3876a417edf70f863031b205b51e2
Martynas Bardauskas <martynasb@wix.com>
2016-11-03 15:24:04 +0200

    animation for pretty box switch

--- src/RNALPrettyBoxSwitch/RNALPrettyBoxSwitch.js
+++ src/RNALPrettyBoxSwitch/RNALPrettyBoxSwitch.js
@@ -4,7 +4,10 @@ import {
   Dimensions,
   Switch,
   Animated,
-  LayoutAnimation
+  LayoutAnimation,
+  StyleSheet,
+  Easing,
+  Text
 } from 'react-native';
 
 export default class RNALPrettyBoxSwitch extends Component {
@@ -13,30 +16,81 @@ export default class RNALPrettyBoxSwitch extends Component {
     super(props);
     this.state = {
       switchValue: false,
-      animationBoxSize : 100,
-      borderRadius: 5000
+      animatedBackgroundScaleSize: new Animated.Value(0),
+      animatedOpacityValue: new Animated.Value(0),
     }
-}
+  }
 
   _onValueChanged(val) {
     this.setState({switchValue: val});
-    LayoutAnimation.spring();
-    this.setState({animationBoxSize: this.state.animationBoxSize + 10})
+    val ? this.animateTo(30) : this.animateTo(0);
+  }
+
+  animateTo(to) {
+    Animated.parallel([
+      Animated.timing(
+        this.state.animatedBackgroundScaleSize,
+        {
+          duration: 300,
+          toValue: to,
+          easing: Easing.linear
+        }
+      ),
+      Animated.timing(
+        this.state.animatedOpacityValue,
+        {
+          duration: this.state.switchValue ? 250 : 300,
+          toValue: this.state.switchValue ? 0 : 1,
+          easing: Easing.linear
+        }
+      )
+    ]).start();
   }
 
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center', position: 'relative'}}>
-        <View style={{position: 'absolute', zIndex: 100}}>
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <Text>Turn this awesome feature on!</Text>
+        </View>
+        <View style={styles.switchContainer}>
           <Switch
             value={this.state.switchValue}
+            onTintColor="#3899ec"
+            tintColor="transparent"
             onValueChange={(val)=> this._onValueChanged(val)}
+            style={styles.switchElement}
           />
+          <Animated.View style={[
+            styles.switchBackground,
+            {
+              opacity: this.state.animatedOpacityValue,
+              transform: [{ scale: this.state.animatedBackgroundScaleSize }]
+            }
+          ]} />
         </View>
-        <View style={{position: 'absolute', zIndex:50, width: this.state.animationBoxSize, height: this.state.animationBoxSize, backgroundColor: 'blue'}}/>
-        <View style={{position: 'absolute', backgroundColor: 'red', top: 0, left: 0, right: 0, bottom: 0, zIndex: 40, borderRadius: 80}}></View>
       </View>
     )
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, flexDirection: 'row', backgroundColor: '#eeeeee', justifyContent: 'center', alignItems: 'center', position: 'relative', overflow: 'hidden'
+  },
+  switchContainer: {
+  },
+  contentContainer: {
+    backgroundColor: 'transparent',
+    zIndex: 10,
+    marginRight: 20
+  },
+  switchElement: {
+    position: 'relative', zIndex: 100, borderColor: '#ccc', borderWidth: 1, borderRadius: 15.5
+  },
+  switchBackground: {
+    position: 'absolute', backgroundColor: '#3899ec', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1,
+    borderRadius: 20,
+  }
+});
